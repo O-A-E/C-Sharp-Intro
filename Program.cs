bbf13411f9d0575bd8cefc36521336edd8ee1453
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-06-24 14:13:31 -0600

    Update 'hello world' text.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace C_Sharp_Intro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, TECHCareers!";
         }
     }
 }
