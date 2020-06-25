0a79d76ce7b4dffe4d616d0d136d8d05d4273900
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-06-25 15:05:12 -0600

    How to do for loops and iteration

--- Program.cs
+++ Program.cs
@@ -6,23 +6,38 @@ namespace C_Sharp_Intro
     {
         static void Main(string[] args)
         {
-         Console.WriteLine( "Enter a number:" );
-            int userInt = Int32.Parse( Console.ReadLine() );
-            if ( userInt > 5 )
+            int myNum = 3; 
+            while ( myNum > 0 ) // iterates or loops while the CONDITION si TRUE.
             {
-                Console.WriteLine( "Greater than 5." );
+                Console.WriteLine( "Current number is: {0}", myNum );
+                myNum--;
+                //everything in these curly braces will continue to run while the condition is true
+                //myNum--the "--"  will reduce the output by one
+                //myNum++ the "++ will increase the outout by one
+
+                //purpose of loops is for any time we need an action to repeat!
             }
-            else if ( userInt > 10 )
+
+            /* For loops always have 3 componentssemi-colon components:
+                    1) we have an assignment
+                    2) [Termination] condition
+                    3) Iteration. (for example ( i += 2) )
+
+                for loops keep your code tidy!
+                
+                integer starts at 0, each time it loops, it checks the 
+                first condition to see if it's true or false
+                if i > 6, it will loop again and run the code block and interate
+
+            
+            */
+            for ( int i = 0; i < 6; i += 2 )
+                    // += means it will add 2 to the original value each step!
             {
-                Console.WriteLine( "Greater than 10." );
-            }
-            else if ( userInt < 0 )
-            {
-                Console.WriteLine( "A negative number." );
-            }
-            else
-            {
-                Console.WriteLine( "Number is between 0 and 5." );
+                Console.WriteLine( "current (for) number is: {0}", i );
             }
         }
-        }}
+
+
+    }
+}
