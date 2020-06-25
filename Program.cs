febc5960b8a80f59a1670217b2adabddb6aec409
O-A-E <67084069+O-A-E@users.noreply.github.com>
2020-06-25 09:18:09 -0600

    Added a field to add name for a client

--- Program.cs
+++ Program.cs
@@ -16,6 +16,28 @@ namespace C_Sharp_Intro
 
            ... and end multi line comments with an asterisk followed by a forward slash */
             Console.WriteLine("Hello, " + myName);
+
+            Console.writeline("Please enter your name...";)
+
+            // Assign variable 'username' to a value entered by the use.
+
+            string username = Console.Readline();
+            // Show user we collected their name.
+            console.writeline("You have entered: " +username);
+            //Instead of traditional concatenation using the addition operator
+            // We can use string interpolatio (place holders) to embed values in
+            // a string!
+            // @link https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/tokens/interpolated
+
+           
+            console.writeline("Hey {0}, you're looking great today!" , username);
+            // Let's play with strings!
+            console.writeline( "makemeuppercase".ToUpper() ); //Convert to uppercase
+            console.writeline("MAKEMELOWERCASE". ToLower() ); //Convert to lowercase
+            // Substring takes two arguments: 1) Starting character position 2) Number of characters to collect.
+            console.writeline( "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Substring( 4, 6  ) );
+
+            console.writeline("XYZ".Length ); // Get the number of characters in the string.
         }
     }
 }
